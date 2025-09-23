025afa7d57d5ad188f3e76d5dfaa2a6528fe8441
Swayam1129 <swayam2@ualberta.ca>
2025-09-23 15:06:14 -0600

    Swayam Branch with Triangle Shape extends Shape

---
+++ app/app/src/main/java/com/example/syntax/Triangle.java
@@ -0,0 +1,29 @@
+package com.example.syntax;
+
+public class Triangle {
+    private int base;
+    private int height;
+
+    public Triangle(int x, int y, base, height){
+        super(x,y);
+        this.base = base;
+        this.height = height;
+    }
+
+    public int getBase() {
+        return base;
+    }
+
+    public void setBase(int base) {
+        this.base = base;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public void setHeight(int height) {
+        this.height = height;
+    }
+
+}
