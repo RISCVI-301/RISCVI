d58a0af70be688b78f0edf33ea3a2cacd0d98ae3
Muhammad Zain <muhammadzainasad6@gmail.com>
2025-09-23 19:37:52 -0600

    Done.

--- app/app/src/main/java/com/example/syntax/Shape.java
+++ app/app/src/main/java/com/example/syntax/Shape.java
@@ -3,6 +3,7 @@ package com.example.syntax;
 public abstract class Shape {
     private int x;
     private int y;
+
     String color = "green";
     public Shape(int x, int y){
         this.x = x;
