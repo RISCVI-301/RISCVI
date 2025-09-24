c6174e82e2c54b048db5633360da0b335ab876dd
Affan <143850755+M-Affan-Nazir@users.noreply.github.com>
2025-09-23 19:35:12 -0600

    Update Shape.java

--- app/app/src/main/java/com/example/syntax/Shape.java
+++ app/app/src/main/java/com/example/syntax/Shape.java
@@ -3,6 +3,8 @@ package com.example.syntax;
 public abstract class Shape {
     private int x;
     private int y;
+
+    String color = "purple";
     
     public Shape(int x, int y){
         this.x = x;
