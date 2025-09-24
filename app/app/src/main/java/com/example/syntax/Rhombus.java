cde44c47d850fb207a1c245228ad0044256d2533
Muhammad Salaar Butt <msbutt112004@gmail.com>
2025-09-23 18:24:00 -0600

    Added a Rhombus class that extends shape

---
+++ app/app/src/main/java/com/example/syntax/Rhombus.java
@@ -0,0 +1,14 @@
+package com.example.syntax;
+
+public class Rhombus extends Shape{
+
+    int angle1;
+    int angle2;
+
+
+
+    public Rhombus(int x, int y, int angle1, int angle2) {
+        super(x, y);
+
+    }
+}
