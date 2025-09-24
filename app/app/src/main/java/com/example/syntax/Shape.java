73e38f312395ee076ba6a54206eebd3d55eaa088
Muhammad Salaar Butt <msbutt112004@gmail.com>
2025-09-23 18:40:04 -0600

    Added a String color field

--- app/app/src/main/java/com/example/syntax/Shape.java
+++ app/app/src/main/java/com/example/syntax/Shape.java
@@ -4,7 +4,8 @@ public abstract class Shape {
     private int x;
     private int y;
 
-    String color = "Purple";
+    String color= "Green";
+
     public Shape(int x, int y){
         this.x = x;
         this.y = y;
