ed4d80933e50b367d5cee0ac90363b123e4c4208
Sanika Verma <sanikaverma2019@gmail.com>
2025-09-23 19:13:21 -0600

    Changed color to turquoise

--- app/app/src/main/java/com/example/syntax/Shape.java
+++ app/app/src/main/java/com/example/syntax/Shape.java
@@ -4,7 +4,7 @@ public abstract class Shape {
     private int x;
     private int y;
 
-    String color = "Purple";
+    String color = "Turquoise";
     public Shape(int x, int y){
         this.x = x;
         this.y = y;
