ab7a4bacdb6d4775d5c4654be36f09090e4dac15
Swayam1129 <swayam2@ualberta.ca>
2025-09-26 12:28:26 -0600

    Add Color

--- app/app/src/main/java/com/example/syntax/Shape.java
+++ app/app/src/main/java/com/example/syntax/Shape.java
@@ -4,7 +4,7 @@ public abstract class Shape {
     private int x;
     private int y;
 
-    String color = "green";
+    String color = "Purple";
     public Shape(int x, int y){
         this.x = x;
         this.y = y;
